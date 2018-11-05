7ed8138edfe451e389562c14abf9d40db8640527
DESKTOP-Q4BKBCT\murata <m.murata1982@gmail.com>
2018-11-05 10:41:13 +0900

    no message

--- read_data.py
+++ read_data.py
@@ -330,8 +330,8 @@ def deform_times(path_to_observation_hour_csv = "../data/obsevationhour%03d.csv"
 
 def main():  
     print("start main")
-    remove_no_eruption_period(days_period=30, if_save=True)
-#    df = combine_muogram(if_save=True)
+#    remove_no_eruption_period(days_period=30, if_save=True)
+    df = combine_muogram(if_save=True)
 #    check_timedelta(path_to_image_csv = "../data/1-6.2014-2017.csv")
 #    reform_muogram(path_to_image_csv = "../data/1-6.2014-2017.csv")
 #    make_observation_csv()
